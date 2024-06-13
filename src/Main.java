b35c9dbd5292bc538feabed4918cb8c4ac83d0ee
Uriel Campaña <urielcamp@gmail.com>
2024-06-13 15:21:34 -0300

    push 1r ejercicio

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
 
+        //Ejercicio 1
+
+        LinkedList<String> listaString = new LinkedList<String>();
+
+        listaString.add("Hola");
+        listaString.add("chau");
+        listaString.add("otro texto");
+
+        for (String l: listaString){
+            System.out.println(l);
+        }
 
 
 
